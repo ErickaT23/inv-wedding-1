a0f2cf952b080513496ae4e4f15bc48c2e2bf6e8
Ericka Tuchan <125705781+ErickaT23@users.noreply.github.com>
2024-07-08 19:44:26 -0600

    music

--- Js/musica.js
+++ Js/musica.js
@@ -2,36 +2,35 @@ document.addEventListener("DOMContentLoaded", function() {
     var audio = document.getElementById("audioPlayer");
     var playPauseButton = document.getElementById("playPauseButton");
     var iconoPlayPause = document.getElementById("iconoPlayPause");
+    var initials = document.getElementById("initials");
 
-    // Intentar reproducir el audio automáticamente al cargar la página
-    var playPromise = audio.play();
-
-    if (playPromise !== undefined) {
-        playPromise.then(function() {
-            console.log('Autoplay started successfully');
-            iconoPlayPause.classList.remove("fa-play");
-            iconoPlayPause.classList.add("fa-pause");
-        }).catch(function(error) {
-            console.log('Autoplay failed: ', error);
-            iconoPlayPause.classList.add("fa-play");
+    // Asegura que el audio esté completamente cargado antes de permitir interacciones
+    audio.addEventListener('canplaythrough', function() {
+        // Activar los eventos de clic una vez que el audio esté listo
+        initials.addEventListener("click", function() {
+            togglePlayPause();
         });
-    }
 
-    // Agregar event listener para el botón de play/pause
-    playPauseButton.addEventListener("click", function() {
-        if (audio.paused) {
-            audio.play();
-            iconoPlayPause.classList.remove("fa-play");
-            iconoPlayPause.classList.add("fa-pause");
-        } else {
-            audio.pause();
-            iconoPlayPause.classList.add("fa-play");
-            iconoPlayPause.classList.remove("fa-pause");
+        playPauseButton.addEventListener("click", function() {
+            togglePlayPause();
+        });
+
+        // Función para alternar entre reproducir y pausar
+        function togglePlayPause() {
+            if (audio.paused) {
+                audio.play().then(() => {
+                    // Cambiar el icono a "pausa" cuando el audio comience a reproducirse
+                    iconoPlayPause.classList.remove("fa-play");
+                    iconoPlayPause.classList.add("fa-pause");
+                }).catch(error => {
+                    console.log('Playback failed: ', error);
+                });
+            } else {
+                audio.pause();
+                // Cambiar el icono a "play" cuando el audio esté pausado
+                iconoPlayPause.classList.add("fa-play");
+                iconoPlayPause.classList.remove("fa-pause");
+            }
         }
     });
 });
-
-
-
-
-
