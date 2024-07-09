b1f12e03201dea2f6dff2d91b29f881e1e6a25cd
Ericka Tuchan <125705781+ErickaT23@users.noreply.github.com>
2024-07-08 21:01:34 -0600

    url personalizado

---
+++ Js/invitados.js
@@ -0,0 +1,19 @@
+const invitados = [
+    {
+        nombre: "Juan Perez",
+        adultos: 2,
+        ninos: 1
+    },
+    {
+        nombre: "Maria Lopez",
+        adultos: 1,
+        ninos: 0
+    },
+    {
+        nombre: "Carlos Rodriguez",
+        adultos: 3,
+        ninos: 2
+    }
+];
+
+export default invitados;
--- Js/musica.js
+++ Js/musica.js
@@ -1,39 +1,3 @@
-/*document.addEventListener("DOMContentLoaded", function() {
-    var audio = document.getElementById("audioPlayer");
-    var playPauseButton = document.getElementById("playPauseButton");
-    var iconoPlayPause = document.getElementById("iconoPlayPause");
-    var initials = document.getElementById("initials");
-
-    // Asegura que el audio esté completamente cargado antes de permitir interacciones
-    audio.addEventListener('canplaythrough', function() {
-        // Activar los eventos de clic una vez que el audio esté listo
-        initials.addEventListener("click", function() {
-            togglePlayPause();
-        });
-
-        playPauseButton.addEventListener("click", function() {
-            togglePlayPause();
-        });
-
-        // Función para alternar entre reproducir y pausar
-        function togglePlayPause() {
-            if (audio.paused) {
-                audio.play().then(() => {
-                    // Cambiar el icono a "pausa" cuando el audio comience a reproducirse
-                    iconoPlayPause.classList.remove("fa-play");
-                    iconoPlayPause.classList.add("fa-pause");
-                }).catch(error => {
-                    console.log('Playback failed: ', error);
-                });
-            } else {
-                audio.pause();
-                // Cambiar el icono a "play" cuando el audio esté pausado
-                iconoPlayPause.classList.add("fa-play");
-                iconoPlayPause.classList.remove("fa-pause");
-            }
-        }
-    });
-});*/
 
 document.addEventListener("DOMContentLoaded", function() {
     var audio = document.getElementById("audioPlayer");
